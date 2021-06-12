c0a2264d81380b34b326b2e685d7826a81c2e1bc
Karthick11dec <73515462+Karthick11dec@users.noreply.github.com>
2021-06-12 07:46:41 +0530

    complete

--- index.js
+++ index.js
@@ -3,84 +3,121 @@ const mongodb = require('mongodb');
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const mongoClient = mongodb.MongoClient;
-
-// const { mailOptione, transporter } = require('./mail');
-
 dotenv.config();
-const DB_URL = process.env.DB_URL;
-const DATA_BASE = process.env.DATA_BASE;
-const COLLECTION = 'otp';
-const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-const transporter = nodemailer.createTransport({
-	service: 'gmail',
-	auth: {
-		user: process.env.EMAIL,
-		pass: process.env.PASSWORD,
-	},
-});
+// const DBURL = 'mongodb://127.0.0.1:27017';
+const DBURL = process.env.DBURL || 'mongodb://127.0.0.1:27017' ;
+const DATABASE = "Otp_Manager";
+const COLLECTION = 'Otp';
+const PORT = process.env.PORT || 5000;
 
-const mailOptione = {
-	from: process.env.EMAIL,
-	subject: 'OTP Manager',
-	text: 'Hi sir/madam',
-};
+const mongoClient = mongodb.MongoClient;
+
+//opt is for control the registeration, before creating an account with any apps
+
+const compare = (t1,t2) => {
+
+ 	let m = Math.abs(parseInt(t2[0]) - parseInt(t1[0]));
+
+	var a = "";
+
+	if (m === 0) {
+		if (t1[1] < t2[1] || t1[1] === t2[1] ) {
+			let r = t2[1] - t1[1]
+			if (r <= 5 && t1[3] === t2[3]) {
+				a = "valid";
+			}
+			else {
+				a = "invalid";
+			}
+		}
+	}
+	else if (m === 1) {
+		if ((t2[1] === "01" || t2[1] === "02" || t2[1] === "03" || t2[1] === "04") && t1[3] === t2[3] )
+		{
+			let h = 60 - parseInt(t1[1]);
+			let a = h + parseInt(t2[1]);
+			a = "valid";
+		}
+		else {
+			a = "invalid";
+		}
+	}
+	return a;
+}
+// compare([ '10', '37', '41', 'PM' ], [ '10', '38', '58', 'PM' ])
+
+const validate = () => {
+	let time = new Date().toLocaleTimeString().split(":");
+	let [h, m, s] = time;
+	let maradian = s.split(" ");
+	const full = [h, m, maradian].flat(1);
+	// console.log(full)
+	return full;
+}
+// validate()
 
 app.post('/generate', async (req, res) => {
 	try {
-		const client = await mongoClient.connect(DB_URL);
-		const db = client.db(DATA_BASE);
-		await db.collection(COLLECTION).createIndex({ createdAt: 1 }, { expireAfterSeconds: 300 });
-		console.log('index created');
+		const client = await mongoClient.connect(DBURL);
+		const db = client.db(DATABASE);
 		const user = await db.collection(COLLECTION).findOne({ email: req.body.email });
 		if (!user) {
 			const otp = Math.random().toString(10).split('.')[1].slice(0, 6);
+			await db.collection(COLLECTION).insertOne({ time: validate(), email: req.body.email, otp: otp });
+			const transporter = nodemailer.createTransport({
+				service: 'gmail',
+				auth: {
+					user: process.env.EMAIL,
+					pass: process.env.PASSWORD,
+				},
+			});
 
-			await db
-				.collection(COLLECTION)
-				.insertOne({ createdAt: new Date(), email: req.body.email, otp: otp });
-			mailOptione.to = req.body.email;
-			mailOptione.html = `
-         <p>Your OTP is for OTP Manager App is</p>
-         <h3>${otp}</h3>
-         <p>this OTP will be valid only 5 minutes at the time of getting this mail</p>`;
-			transporter.sendMail(mailOptione, (err, data) => {
-				if (err) {
-					console.log(err);
-				} else {
-					console.log('Email Sent');
+			const mailOption = {
+				from: process.env.EMAIL,
+				to: req.body.email,
+				subject: 'OTP Manager',
+				text: 'Hi sir/madam',
+				html: `
+                       <p>Your OTP is for OTP Manager App is</p>
+                       <h3>${otp}</h3>
+                       <p>this OTP will be valid only <b>5 minutes</b> at the time of getting this mail</p>`
+			};
+			transporter.sendMail(mailOption, (err, data) => {
+				if (data) {
+					res.status(200).json({ message: 'created', data });
+				} else if (err) {
+					res.status(400).json({ err })
 				}
 			});
-			res.status(200).json({ message: 'created' });
 		} else {
-			console.log('User already exixt');
+			console.log('User already exist');
 		}
 	} catch (error) {
-		console.log(error);
 		res.status(400).json({ message: 'something went wrong' });
 	}
 });
 
+
 app.post('/verify', async (req, res) => {
 	try {
-		const client = await mongoClient.connect(DB_URL);
-		const db = client.db(DATA_BASE);
+		const client = await mongoClient.connect(DBURL);
+		const db = client.db(DATABASE);
 		const user = await db.collection(COLLECTION).findOne({ email: req.body.email });
 		if (user) {
-			const record = await db.collection(COLLECTION).findOne({ email: req.body.email });
-			if (record.otp === req.body.otp) {
+			if (user.otp === req.body.otp && compare(user.time, req.body.time) ) {
 				await db.collection(COLLECTION).deleteOne({ email: req.body.email });
 				res.status(200).json({ message: 'OTP Matched', result: true });
 			} else {
-				res.status(400).json({ message: 'Entered OTP is wrong', result: false });
+				res.status(400).json({ message: 'Entered OTP is wrong or Otp timeOut', result: false });
 			}
 		} else {
-			res.status(400).json({ message: 'Incorrect Email Id or the OTP expires', result: false });
+			res.status(400).json({ message: 'Incorrect Email Id (or) OTP Already used once', result: false });
 		}
 	} catch (error) {
 		console.log(error);
